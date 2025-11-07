a8a0ffc34e72f3f01f5aea04b95101d358ee9a3b
Youri Tils <youri.tils@nxp.com>
2025-11-07 12:08:08 +0100

    Added support for stepper motor in flexpwm driver
    - Added support for stepper motor in allpwm(), twoChannelForward() and twoChannelReverse()
    - Cleaned up phaseouts.c and added more comments

--- Mcu/a153/Src/phaseouts.c
+++ Mcu/a153/Src/phaseouts.c
@@ -30,39 +30,6 @@ extern char prop_brake_active;
 
 //#define INVERT_PWM
 
-/*
- * @brief 	Sets high FET output pins to LOW and enables PWM on the low FET output pins.
- * 			This causes the motor to brake and the brake strength is then controlled by the duty cycle.
- */
-void proportionalBrake()
-{
-	//Set source using DTSRCSEL
-#ifdef INVERT_PWM
-	modifyReg16(&FLEXPWM0->DTSRCSEL, 0xfff,
-			PWM_DTSRCSEL_SM0SEL23(0) | PWM_DTSRCSEL_SM1SEL23(0) | PWM_DTSRCSEL_SM2SEL23(0));
-#else
-	modifyReg16(&FLEXPWM0->DTSRCSEL, 0xfff,
-			PWM_DTSRCSEL_SM0SEL23(1) | PWM_DTSRCSEL_SM1SEL23(1) | PWM_DTSRCSEL_SM2SEL23(1));
-#endif
-
-	//Mask high FETs so its LOW, PWM_B are the high FETS
-	modifyReg16(&FLEXPWM0->MASK, 0x777, 0x070);
-
-	//Force out event
-	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
-}
-
-void allOff()
-{
-	//A FLOAT, B FLOAT, C FLOAT
-
-	//Mask all phases
-	modifyReg16(&FLEXPWM0->MASK, 0x777, 0x770);
-
-	//Force out event
-	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
-}
-
 void comStep(char newStep)
 {
     switch (newStep) {
@@ -169,6 +136,7 @@ void comStep(char newStep)
 
 void fullBrake()
 { // full braking shorting all low sides
+	//A LOW, B LOW, C LOW
 
 	//Set source using DTSRCSEL
 #ifdef INVERT_PWM
@@ -183,24 +151,89 @@ void fullBrake()
 	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
 }
 
-//TODO the following are never called
+/*
+ * @brief 	Sets high FET output pins to LOW and enables PWM on the low FET output pins.
+ * 			This causes the motor to brake and the brake strength is then controlled by the duty cycle.
+ */
+void proportionalBrake()
+{
+	//Set source using DTSRCSEL
+#ifdef INVERT_PWM
+	modifyReg16(&FLEXPWM0->DTSRCSEL, 0xfff,
+			PWM_DTSRCSEL_SM0SEL23(0) | PWM_DTSRCSEL_SM1SEL23(0) | PWM_DTSRCSEL_SM2SEL23(0));
+#else
+	modifyReg16(&FLEXPWM0->DTSRCSEL, 0xfff,
+			PWM_DTSRCSEL_SM0SEL23(1) | PWM_DTSRCSEL_SM1SEL23(1) | PWM_DTSRCSEL_SM2SEL23(1));
+#endif
+
+	//Mask high FETs so its LOW, PWM_B are the high FETS
+	modifyReg16(&FLEXPWM0->MASK, 0x777, 0x070);
+
+	//Force out event
+	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
+}
+
+/*
+ * @brief 	Turn off all FETs so all are floating
+ */
+void allOff()
+{
+	//A FLOAT, B FLOAT, C FLOAT
+
+	//Mask all phases
+	modifyReg16(&FLEXPWM0->MASK, 0x777, 0x770);
+
+	//Force out event
+	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
+}
+
 void allpwm()
 { // for stepper_sine
-//    phaseAPWM();
-//    phaseBPWM();
-//    phaseCPWM();
+	//A PWM, B PWM, C PWM
+
+	//Set source using DTSRCSEL
+#ifdef INVERT_PWM
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+	    	PWM_DTSRCSEL_SM0SEL23(0) | PWM_DTSRCSEL_SM1SEL23(0) | PWM_DTSRCSEL_SM2SEL23(0));
+#else
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+			PWM_DTSRCSEL_SM0SEL23(1) | PWM_DTSRCSEL_SM1SEL23(1) | PWM_DTSRCSEL_SM2SEL23(1));
+#endif
+
+	//Force out event
+	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
 }
 
 void twoChannelForward()
 {
-//    phaseAPWM();
-//    phaseBLOW();
-//    phaseCPWM();
+	//A PWM, B LOW, C PWM
+
+	//Set source using DTSRCSEL
+#ifdef INVERT_PWM
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+	    	PWM_DTSRCSEL_SM0SEL23(0) | PWM_DTSRCSEL_SM1SEL23(2) | PWM_DTSRCSEL_SM2SEL23(0));
+#else
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+			PWM_DTSRCSEL_SM0SEL23(1) | PWM_DTSRCSEL_SM1SEL23(2) | PWM_DTSRCSEL_SM2SEL23(1));
+#endif
+
+	//Force out event
+	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
 }
 
 void twoChannelReverse()
 {
-//    phaseALOW();
-//    phaseBPWM();
-//    phaseCLOW();
+	//A LOW, B PWM, C LOW
+
+	//Set source using DTSRCSEL
+#ifdef INVERT_PWM
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+	    	PWM_DTSRCSEL_SM0SEL23(2) | PWM_DTSRCSEL_SM1SEL23(0) | PWM_DTSRCSEL_SM2SEL23(2));
+#else
+	modifyReg16(&FLEXPWM0->DTSRCSEL, PWM_DTSRCSEL_SM0SEL23_MASK | PWM_DTSRCSEL_SM1SEL23_MASK | PWM_DTSRCSEL_SM2SEL23_MASK,
+			PWM_DTSRCSEL_SM0SEL23(2) | PWM_DTSRCSEL_SM1SEL23(1) | PWM_DTSRCSEL_SM2SEL23(2));
+#endif
+
+	//Force out event
+	modifyReg16(&FLEXPWM0->SM[0].CTRL2, 0, PWM_CTRL2_FORCE(1));
 }
