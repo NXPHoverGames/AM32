b2f672de0a16a672ca14e8b7a604023e073cb1b1
Youri Tils <youri.tils@nxp.com>
2025-11-07 10:32:51 +0100

    Updated MCXA153 common register function
    - Made modifyReg functions inline functions and put them in header file for improved processing speed.

--- Mcu/a153/Inc/mcxa153_common.h
+++ Mcu/a153/Inc/mcxa153_common.h
@@ -10,8 +10,28 @@
 
 #include "main.h"
 
-void modifyReg32(volatile uint32_t *regAddr, uint32_t clearbits, uint32_t setbits);
-void modifyReg16(volatile uint16_t *regAddr, uint16_t clearbits, uint16_t setbits);
-void modifyReg8(volatile uint8_t *regAddr, uint8_t clearbits, uint8_t setbits);
+inline void modifyReg32(volatile uint32_t *regAddr, uint32_t clearbits, uint32_t setbits)
+{
+	uint32_t reg = *regAddr;
+	reg &= ~clearbits;
+	reg |= setbits;
+	*regAddr = reg;
+}
+
+inline void modifyReg16(volatile uint16_t *regAddr, uint16_t clearbits, uint16_t setbits)
+{
+	uint16_t reg = *regAddr;
+	reg &= ~clearbits;
+	reg |= setbits;
+	*regAddr = reg;
+}
+
+inline void modifyReg8(volatile uint8_t *regAddr, uint8_t clearbits, uint8_t setbits)
+{
+	uint8_t reg = *regAddr;
+	reg &= ~clearbits;
+	reg |= setbits;
+	*regAddr = reg;
+}
 
 #endif /* MCU_A153_INC_MCXA153_COMMON_H_ */
